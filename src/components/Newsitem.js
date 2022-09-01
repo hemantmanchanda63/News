39e238b1892449c32e1e842298c5d2cf12d6be62
Himanshu Manchanda <41925126+hemantmanchanda63@users.noreply.github.com>
2022-09-02 00:19:55 +0530

    dn

---
+++ src/components/Newsitem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Newsitem = (props) => {
+  const { title, description, imgurl } = props;
+  return (
+    <>
+      <div className="card mb-3" style={{ width: "18rem" }}>
+        <img src={imgurl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/" className="btn btn-primary btn-xl">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Newsitem;
